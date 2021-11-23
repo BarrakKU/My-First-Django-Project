b8d3694f7585ce05ba50149d892a118a234f4946
BarrakKU <barrak.babtain@cba.ku.edu.kw>
2021-11-23 10:06:35 +0300

    q

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def test_view(request):
+    print(f"Request: {request}")
+
+    name = "Barrak"
+
+    return HttpResponse(f"""<html><body>
+  <h1>Hello {name}!</h1>
+  <p>This is my first <strong>html</strong> web app!</p>
+  </body></html>""")
